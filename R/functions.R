fc628759d1c100d7fafad30337cf6d2e09deca44
Julien Racle <julien.racle@unil.ch>
2024-06-04 17:50:37 +0200

    Corrected a small bug in merge_mouse_TRAV in case the input x was a tibble instead of data.frame.

--- R/functions.R
+++ R/functions.R
@@ -4,7 +4,7 @@
 #####
 
 build_stat <- function(es, chain.list, sp){
-  
+
   L <- list()
   countL <- list()
   countV <- list()
@@ -16,21 +16,21 @@ build_stat <- function(es, chain.list, sp){
   countCDR2 <- list()
   countCDR3.L <- list()
   countVJ.L <- list()
-  
+
   for(chain in chain.list){
-    
+
     seg <- c(paste(chain,"V", sep=""),paste(chain,"J", sep=""))
     cdr3 <- paste("cdr3_",chain,sep="")
-    
+
     countV[[chain]] <- table(es[,seg[1]])
     countJ[[chain]] <- table(es[,seg[2]])
-    
+
     countL[[chain]] <- table(nchar(es[,cdr3]))
-    
+
     L[[chain]] <- as.numeric(names(countL[[chain]]))
 
     countVJ[[chain]] <- table(es[,seg[1]], es[,seg[2]])
-    
+
     countV.L[[chain]] <- list()
     countJ.L[[chain]] <- list()
     countVJ.L[[chain]] <- list()
@@ -40,25 +40,25 @@ build_stat <- function(es, chain.list, sp){
       countJ.L[[chain]][[lg]] <- table(es[ind,paste(chain,"J",sep="")])
       countVJ.L[[chain]][[lg]] <- table(es[ind,paste(chain,"V",sep="")],es[ind,paste(chain,"J",sep="")])
     }
-    
+
     if(length(countV[[chain]])>0){
       countCDR1[[chain]] <- count_aa(cdr123[[sp]][[chain]][es[,seg[1]],"CDR1"], keep.gap=1)
       countCDR2[[chain]] <- count_aa(cdr123[[sp]][[chain]][es[,seg[1]],"CDR2"], keep.gap=1)
     }
     countCDR3.L[[chain]] <- count_aa(es[,cdr3], keep.gap=0)
-    
-    
+
+
   }
   count <- list(L, countL, countV, countJ, countV.L, countJ.L, countCDR1, countCDR2, countCDR3.L, countVJ, countVJ.L)
   names(count) <- c("L", "countL", "countV", "countJ", "countV.L", "countJ.L", "countCDR1", "countCDR2", "countCDR3.L", "countVJ", "countVJ.L")
-  
+
   return(count)
 }
 
 #Compute the counts of each aa at each position.
 #Would be better to have the option of treating gaps in CDR1/2 as separate amino acids, while missing data can be treated as 'unspecific'
 count_aa <- function(x, keep.gap=0){
-  
+
   if(keep.gap == 0){    #All gaps, including "g" are treated as unspecific data. This can be useful for visualisation.
     tgap <- c(gap,"g")
     taa.list <- aa.list
@@ -66,7 +66,7 @@ count_aa <- function(x, keep.gap=0){
     tgap <- c()
     taa.list <- c(aa.list,"g")
   }
-  
+
   #First get the list of length
   lc <- nchar(x)
   lg <- sort(unique(lc))
@@ -101,7 +101,7 @@ count_aa <- function(x, keep.gap=0){
 
 #This assumes the matrix includes gaps (21 rows)
 build_cdr12_motif <- function(x, keep.gap=0){
-  
+
   if(keep.gap==0){
     g <- x["g",]
     for(a in aa.list){
@@ -116,20 +116,20 @@ build_cdr12_motif <- function(x, keep.gap=0){
 #Compute the weighted average of VJ usage for different CDR3 lengths
 #This is no longer useful.
 weighted_VJcount <- function(x,y){
-  
+
   #Build the list of all segment names
   nm <- c()
   for(i in names(y)){
     nm <- c(nm, names(x[[as.numeric(i)]]) )
   }
   nm <- unique(nm)
-  
+
   ct <- rep(0, length(nm))
   names(ct) <- nm
   for(i in names(y)){
     v <- x[[as.numeric(i)]]
     v <- v/sum(v)  #This is the normalized distribution of V or J for a given length
-    
+
     for(j in 1:length(v)){
       ct[names(v)[j]] <- ct[names(v)[j]] + v[j]*y[i]
     }
@@ -141,7 +141,7 @@ weighted_VJcount <- function(x,y){
 # This is important since the choice of V/J has huge influence on the CDR3 motif.
 # By considering V/J usage in baseline, it helps identifying what is specific to the input TCR
 weighted_countCDR3 <- function(x,y){
-  
+
   countCDR3 <- list()
   L <- which(y != "NULL")
   for(lg in L){
@@ -166,7 +166,7 @@ weighted_countCDR3 <- function(x,y){
       countCDR3[[lg]] <- scale(countCDR3[[lg]],center=F, scale=colSums(countCDR3[[lg]]))
     } else {
       countCDR3[[lg]][1:N.aa,1:lg] <- 0.05 #This is the case if the were no data for all V-J for this lenght in the repertoire
-      
+
     }
   }
   return(countCDR3)
@@ -175,10 +175,10 @@ weighted_countCDR3 <- function(x,y){
 # Compute the weighted average of CDR3 length for the observed V/J usage
 # This is important since CDR3 length is primarily determined by the length of the V and J segments
 weighted_countL <- function(x,y){
-  
+
   countL <- rep(0,times=Lmax-Lmin+1)
   names(countL) <- as.character(Lmin:Lmax)
-  
+
   y <- y/sum(y)
   for(v in rownames(y)){
     for(j in colnames(y)){
@@ -202,7 +202,7 @@ weighted_countL <- function(x,y){
 #The input should be a matrix/dataframe with the MHC columns and a specices column, since it only works for human
 #This is only needed for the MixTCRpred1.0 input.
 find_mhc <- function(m){
-  mhc <- unlist(lapply(1:dim(m)[1], function(x){ 
+  mhc <- unlist(lapply(1:dim(m)[1], function(x){
     if(m[x,"MHC"]=="HLA-A:01") {h <- "A0101"}
     else{
       if(m[x,"species"] == "HomoSapiens"){
@@ -218,10 +218,10 @@ find_mhc <- function(m){
   }))
 }
 
-# Plot the comparison between V or J usage. 
+# Plot the comparison between V or J usage.
 # We should have different criteria for selecting the labels for the ES versus Pred
 plotVJ <- function(count.es, count.rep, info, comp.baseline){
-  
+
   #count.es is on the Y axis, count.rep on the X
   gene <- info[1]
   n <- sum(count.es)
@@ -235,19 +235,19 @@ plotVJ <- function(count.es, count.rep, info, comp.baseline){
   rownames(count) <- nm
   colnames(count) <- cn
   for(i in 1:length(count.es)){ count[names(count.es[i]),type1] <- count.es[i] }
-  for(i in 1:length(count.rep)){ count[names(count.rep[i]),type2] <- count.rep[i] } 
+  for(i in 1:length(count.rep)){ count[names(count.rep[i]),type2] <- count.rep[i] }
   count <- scale(count, center=F, scale=colSums(count))
   count.df <- data.frame(count)
   colnames(count.df) <- c("Y","X")
-  
+
   lim.y <- max(count[,c(type1)] )*1.3
   lim.x <- max(count[,c(type2)] )*1.3
-  
+
   #lim.y <- round(min( max(count[,c(type1)] )+0.1, max( count[,c(type1)] )*1.4),1)
   #lim.x <- min(max(count[,c(type2)])+0.1, max(count[,c(type2)])*1.4)
   #if(lim.x < 0.1){lim.x <- round(lim.x+0.005,2)}
   #else { lim.x <- round(lim.x+0.05,1) }
-  
+
   label <- nm
   label[count.df[,"Y"] < 0.05 & count.df[,"X"] < 0.05] <- NA
   ratio <- count.df[,"Y"]/(count.df[,"X"]+0.001)
@@ -256,42 +256,42 @@ plotVJ <- function(count.es, count.rep, info, comp.baseline){
   if(length(which(!is.na(label)))>8){
     label[which( (ratio < 2) & count.df[,"Y"]<0.1)] <- NA
   }
-  
+
   ylab <- paste(type1," (",n,")", sep="")
-  
+
   if(comp.baseline==1){  xlab <- type2 } else {xlab <- paste(type2," (",n.rep,")", sep="")}
-  
+
   #Plot the comparison between input and repertoires
   count.plot <- list()
   if(length(count.es)>0){
-    count.plot <- ggplot(count.df, aes(x=X, y=Y, label=label)) + 
-      geom_point() + geom_abline(col="orange",linetype="dashed",linewidth=1) + 
+    count.plot <- ggplot(count.df, aes(x=X, y=Y, label=label)) +
+      geom_point() + geom_abline(col="orange",linetype="dashed",linewidth=1) +
       ggtitle(gene) +
-      xlim(0, lim.x) + ylim(0,lim.y) + 
+      xlim(0, lim.x) + ylim(0,lim.y) +
       theme(plot.title = element_text(size = 14, hjust=0.5), axis.text=element_text(size=10), axis.title=element_text(size=14)) +
       geom_label_repel(size = 3, nudge_y=0.02, box.padding = 0.15) +
       xlab(xlab) + ylab(ylab)
   } else {count.plot <- ggplot()}
-  
+
   return(count.plot)
-  
+
 }
 
 
 correct.VJnames <- function(es.all, name.list, segment.list){
-  
-  
+
+
   for(sp in species.list){
     for(s in segment.list){
-      
+
       ind <- which(es.all[,s] %in% name.list[[sp]]==F & es.all[,"species"]==sp)
       if(length(ind)>0){
         nm <- strsplit(es.all[ind,s], split="*", fixed=T)
         gene <- unlist(lapply(nm, function(x){x[1]}))
         allele <- unlist(lapply(nm, function(x){x[2]}))
-        
+
         ga <- unlist(lapply(1:length(gene), function(x){ clean.name.allele(gene[x],allele[x],sp)}))
-        
+
         #Check the cases where the segment was not NA, but was put to NA (i.e., mapping of gene name failed)
         i <- which(es.all[ind,s] != "" & is.na(ga)==T)
         if(length(i)>0){
@@ -307,13 +307,13 @@ correct.VJnames <- function(es.all, name.list, segment.list){
 
 
 plotLD <- function(lc.es,lc.rep,info, plot.oneline){
-  
+
   l.all <- sort(as.numeric(unique(c(names(lc.es), names(lc.rep)))))
-  
+
   ct <- 1
   ld.es <- c()
   ld.rep <- c()
-  
+
   for(l in as.character(l.all)){
     if(!is.na(lc.es[l])){ld.es[ct] <- lc.es[l]} else {ld.es[ct] <- 0}
     if(!is.na(lc.rep[l])){ld.rep[ct] <- lc.rep[l]} else {ld.rep[ct] <- 0}
@@ -321,17 +321,17 @@ plotLD <- function(lc.es,lc.rep,info, plot.oneline){
   }
   ld.es <- ld.es/sum(ld.es)
   ld.rep <- ld.rep/sum(ld.rep)
-  
+
   ##########
   #Plot the comparison for length distribution
   ##########
-  
-  v1 <- c(l.all,l.all); 
+
+  v1 <- c(l.all,l.all);
   v2 <- c(ld.es, ld.rep);
-  v3 <- c( rep(info[2], length(l.all)), rep(info[3], length(l.all))) ; 
+  v3 <- c( rep(info[2], length(l.all)), rep(info[3], length(l.all))) ;
   ld.df <- data.frame(v1,v2,v3)
   ld.df$v3 <- factor(ld.df$v3, levels=c(info[2], info[3]))
-  
+
   legend.size <- 12
   if(plot.oneline==1){
     if(nchar(info[2])>23){legend.size=11}
@@ -340,30 +340,30 @@ plotLD <- function(lc.es,lc.rep,info, plot.oneline){
   ld.plot <-  ggplot(ld.df) + geom_point(aes(x=v1, y=v2, color=v3)) + #ggtitle(paste("N =",sum(lc.es))) +
     geom_line(aes(x=v1, y=v2, color=v3)) +
     theme(legend.key.size = unit(0.2, 'cm'), legend.position="top", legend.title=element_blank(),  legend.text=element_text(size=legend.size)) +
-    xlab(paste("Length_CDR3",info[1],sep="")) + ylab("") + 
+    xlab(paste("Length_CDR3",info[1],sep="")) + ylab("") +
     guides(color = guide_legend(nrow = 2)) +
     theme(axis.text=element_text(size=12), axis.title=element_text(size=14), plot.title = element_text(size=15,hjust = 0.5))
-    
+
   return(ld.plot)
-  
+
 }
 
 
 plotCDR3 <- function(lc.es, lc.rep, countCDR3.es, countCDR3.rep, info, comp.baseline, plot.oneline=0, plot.logo.length=0){
-  
- 
+
+
   L.TR <- as.numeric(intersect(names(lc.es), names(lc.rep)))
-  
+
   pwm.rep <- list()
   pwm.es <- list()
-  
+
   logo.CDR3.L.es <- list()
   logo.CDR3.L.rep <- list()
-  
+
   tl <- lc.es[as.character(L.TR)]
   lmax.es <- as.numeric(names(tl[which.max(tl)]))
   lmax <- lmax.es
-  
+
   if(plot.oneline==1){
     if(lmax<15){
       axis.size.max <- 8
@@ -376,52 +376,52 @@ plotCDR3 <- function(lc.es, lc.rep, countCDR3.es, countCDR3.rep, info, comp.base
     title.size <- 12
   }
   ylab <- ""
-  
+
 
   if(plot.logo.length==0){
     L.TR <- c(lmax)
   }
-  
+
   for(l in L.TR){
-    
+
     lc <- as.character(l)
-    
+
     pwm.es[[l]] <- scale(countCDR3.es[[l]], center=F, scale=colSums(countCDR3.es[[l]]))
     pwm.rep[[l]] <- scale(countCDR3.rep[[l]], center=F, scale=colSums(countCDR3.rep[[l]]))
-    
+
     title <- paste("CDR3", info[1],"_",l," ",info[2], " (",lc.es[[lc]],")", sep="")
-    logo.CDR3.L.es[[l]] <- ggseqlogoMOD(data=pwm.es[[l]], additionaAA=additionalAA,  axisTextSizeX = 12, axisTextSizeY = 8) + 
+    logo.CDR3.L.es[[l]] <- ggseqlogoMOD(data=pwm.es[[l]], additionaAA=additionalAA,  axisTextSizeX = 12, axisTextSizeY = 8) +
       labs(title=title) + ylab(ylab) + theme(plot.title=element_text(size=15, hjust=0.5))
- 
+
     title.baseline <- paste("CDR3", info[1],"_",l," ",info[3], sep="")
     if(comp.baseline==0){title.baseline <- paste(title.baseline, " (",lc.rep[[lc]],")", sep="")} else {title.baseline <- title.baseline}
-    
+
     logo.CDR3.L.rep[[l]] <- ggseqlogoMOD(data=pwm.rep[[l]], additionaAA=additionalAA,  axisTextSizeX = 12, axisTextSizeY = 8) +
       labs(title=title.baseline) + ylab(ylab) + theme(plot.title=element_text(size=15, hjust=0.5))
-    
+
     #For the special case where l==lmax, build the logo with different graphical parameters, depending on the plot.online
     if(l==lmax){
       title <- paste("CDR3", info[1],"_",l," ",info[2], " (",lc.es[[lc]],")", sep="")
       title.baseline <- paste("CDR3", info[1],"_",l," ",info[3], sep="")
       if(comp.baseline==0){title.baseline <- paste(title.baseline, " (",lc.rep[[lc]],")", sep="")}
-      
+
       if(plot.oneline==1 & (nchar(title)>26 | nchar(title.baseline)>26)){
         title <- paste("CDR3", info[1],"_",l," ",info[2], "\n(",lc.es[[lc]],")", sep="")
         title.baseline <- paste("CDR3", info[1],"_",l," ",info[3],"\n", sep="")
         if(comp.baseline==0){title.baseline <- paste(title.baseline, "(",lc.rep[[lc]],")", sep="")}
       }
-      
-      logo.CDR3.L.es.max <- ggseqlogoMOD(data=pwm.es[[l]], additionaAA=additionalAA,  axisTextSizeX = axis.size.max, axisTextSizeY = 8) + 
+
+      logo.CDR3.L.es.max <- ggseqlogoMOD(data=pwm.es[[l]], additionaAA=additionalAA,  axisTextSizeX = axis.size.max, axisTextSizeY = 8) +
         labs(title=title) + ylab(ylab) + theme(plot.title=element_text(size=title.size, hjust=0.5))
-      
-        
+
+
       logo.CDR3.L.rep.max <- ggseqlogoMOD(data=pwm.rep[[l]], additionaAA=additionalAA,  axisTextSizeX = axis.size.max, axisTextSizeY = 8) +
         labs(title=title.baseline) + ylab(ylab) + theme(plot.title=element_text(size=title.size, hjust=0.5))
     }
-    
-    
+
+
   }
-  
+
   ls <- list(logo.CDR3.L.es, logo.CDR3.L.rep, L.TR, lmax, logo.CDR3.L.es.max, logo.CDR3.L.rep.max)
   names(ls) <- c("ES", "Baseline", "length", "lmax", "ES_max", "Baseline_Max")
   return(ls)
@@ -429,7 +429,7 @@ plotCDR3 <- function(lc.es, lc.rep, countCDR3.es, countCDR3.rep, info, comp.base
 
 
 check_input <- function(x, col.TCR){
-  
+
 
   #Check missing input
   for(cl in c(col.TCR)){
@@ -451,26 +451,26 @@ check_input <- function(x, col.TCR){
     x <- cbind(x,"Model_default")
     colnames(x) <- c(cn, "model")
     print("Missing model information, using the same model (Model_default)")
-    
+
   }
   return(x)
 }
 
 merge_mouse_TRAV <- function(x){
-  
+
   #This has to be run after alleles have been removed and genes have been corrected
-  
+
   if("TRAV" %in% colnames(x)){
     ind <- which(x[,"species"]=="MusMusculus")
-    v.cor <- as.character(unlist(lapply(x[ind,"TRAV"], function(y){
-      if(y %in% names(merge.mouse.TRAV) == T){
+    v.cor <- as.character(unlist(lapply(x[["TRAV"]][ind], function(y){
+      if (y %in% names(merge.mouse.TRAV)){
         y <- merge.mouse.TRAV[y]
       }
       return(y)
     })))
-    x[ind,"TRAV"] <- v.cor 
+    x[ind,"TRAV"] <- v.cor
   }
-  
+
   return(x)
 }
 
