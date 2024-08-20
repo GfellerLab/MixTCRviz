fa4236800f9ab78a0b55a5edaade6f73f396312a
DavidGfeller <davidgfeller@wle-32-104.unil.ch>
2024-08-20 11:26:39 +0200

    test

--- test_MixTCRviz.R
+++ test_MixTCRviz.R
@@ -1,4 +1,12 @@
 
+#Run it from the MixTCRviz folder
+
+#Do this if you do not want to install the package
+devtools::load_all(".")
+
+#Do this if you have installed the package
+library(MixTCRviz)
+
 #Compare input TCRs (specific for A0201_LLWNGPMAV) with baseline repertoire
 MixTCRviz::MixTCRviz(input1="test/test1.csv", output.path="test/out/test1")
 
