e85927254594cc4cf99cfb61c9e2e602d7a59313
DavidGfeller <davidgfeller@adms-macbook-pro.home>
2024-08-24 11:19:37 +0200

    test

--- test_MixTCRviz.R
+++ test_MixTCRviz.R
@@ -10,10 +10,10 @@ devtools::load_all(".")
 #library(MixTCRviz)
 
 #Compare input TCRs (specific for A0201_LLWNGPMAV) with baseline repertoire
-MixTCRviz(input1="test/test1.csv", output.path="test/out/test1")
+MixTCRviz(input1="test/test.csv", output.path="test/out/")
 
-list_1 <- readRDS("test/out/test1/stats/A0201_LLWNGPMAV.rds")
-list_2 <- readRDS("test/out_compare/test1/stats/A0201_LLWNGPMAV.rds")
+list_1 <- readRDS("test/out/stats/A0201_LLWNGPMAV.rds")
+list_2 <- readRDS("test/out_compare/stats/A0201_LLWNGPMAV.rds")
 
 comp <- identical(list_1, list_2)
 if(comp){
