7c8eff0c80ccd78e14c38360401f3448b699feb6
DavidGfeller <davidgfeller@wle-32-176.unil.ch>
2025-02-13 15:57:08 +0100

    update example in test/out_compare for the mouse over

--- test_MixTCRviz.R
+++ test_MixTCRviz.R
@@ -12,12 +12,22 @@ devtools::load_all(".")
 #Compare input TCRs (specific for A0201_LLWNGPMAV) with baseline repertoire
 MixTCRviz(input1="test/test.csv", output.path="test/out/", output.stat = T, interactive.plots = T)
 
-list_1 <- readRDS("test/out/stats/A0201_LLWNGPMAV.rds")
-list_2 <- readRDS("test/out_compare/stats/A0201_LLWNGPMAV.rds")
+new.data <- F
 
-comp <- identical(list_1, list_2)
-if(comp){
-  print("No problem detected")
+if(new.data){
+  
+  system("cp test/out/A0201_LLWNGPMAV* test/out_compare/")
+  system("cp test/out/stats/A0201_LLWNGPMAV.rds test/out_compare/stats/A0201_LLWNGPMAV.rds")
+  
 } else {
-  stop("There were some issues...")
+  
+  list_1 <- readRDS("test/out/stats/A0201_LLWNGPMAV.rds")
+  list_2 <- readRDS("test/out_compare/stats/A0201_LLWNGPMAV.rds")
+  
+  comp <- identical(list_1, list_2)
+  if(comp){
+    print("No problem detected")
+  } else {
+    stop("There were some issues...")
+  }
 }
