45045aed45e9ca1d693ce43a84010cefd6cb378d
DavidGfeller <davidgfeller@adms-macbook-pro-4.home>
2026-06-06 17:48:06 +0200

    README and baseline example in test file

--- test_MixTCRviz.R
+++ test_MixTCRviz.R
@@ -20,13 +20,14 @@ if(comp){
   stop("There were some issues...")
 }
 
-#Test MixTCRviz including V/J allele (not recommended unless the baseline comes from the same donor as the data in input1)
+#Test MixTCRviz including V/J allele (NOT recommended unless the baseline comes from the same donor as the data in input1).
 use.allele <- F
 if(use.allele){
   MixTCRviz(input1="test/test_allele.csv", output.path="test/out/", use.allele=T, model.default = "A0201_LLWNGPMAV_allele")
 }
 
 #Test the MixTCRviz with a specific baseline, including V/J allele information.
+#If input2 is to be used as a baseline, remember to set renormVJ to TRUE.
 use.own.baseline <- F
 if(use.own.baseline){
   MixTCRviz(input1="test/test_allele.csv", input2="test/baseline_allele.csv", output.path="test/out/", use.allele=T, 
