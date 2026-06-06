8cd4b5b63bafedceb55eb0c8ec6c5804698b5862
DavidGfeller <davidgfeller@adms-macbook-pro-4.home>
2026-06-06 17:46:40 +0200

    README and baseline example in test file

--- test_MixTCRviz.R
+++ test_MixTCRviz.R
@@ -10,23 +10,25 @@ library(MixTCRviz)
 #Compare input TCRs (specific for A0201_LLWNGPMAV) with baseline repertoire
 MixTCRviz(input1="test/test.csv", output.path="test/out/", output.stat = T)
 
-new.data <- F
+list_1 <- readRDS("test/out/stats/A0201_LLWNGPMAV.rds")
+list_2 <- readRDS("test/out_compare/stats/A0201_LLWNGPMAV.rds")
 
-if(new.data){
-  
-  system("cp test/out/A0201_LLWNGPMAV* test/out_compare/")
-  system("cp test/out/stats/A0201_LLWNGPMAV.rds test/out_compare/stats/A0201_LLWNGPMAV.rds")
-  
+comp <- identical(list_1, list_2)
+if(comp){
+  print("No problem detected")
 } else {
-  
-  list_1 <- readRDS("test/out/stats/A0201_LLWNGPMAV.rds")
-  list_2 <- readRDS("test/out_compare/stats/A0201_LLWNGPMAV.rds")
-  
-  comp <- identical(list_1, list_2)
-  if(comp){
-    print("No problem detected")
-  } else {
-    stop("There were some issues...")
-  }
-  
+  stop("There were some issues...")
+}
+
+#Test MixTCRviz including V/J allele (not recommended unless the baseline comes from the same donor as the data in input1)
+use.allele <- F
+if(use.allele){
+  MixTCRviz(input1="test/test_allele.csv", output.path="test/out/", use.allele=T, model.default = "A0201_LLWNGPMAV_allele")
+}
+
+#Test the MixTCRviz with a specific baseline, including V/J allele information.
+use.own.baseline <- F
+if(use.own.baseline){
+  MixTCRviz(input1="test/test_allele.csv", input2="test/baseline_allele.csv", output.path="test/out/", use.allele=T, 
+            renormVJ = T, model.default = "A0201_LLWNGPMAV_allele_baseline")
 }
